060d882267fb978c7d50ff76fedd0358e4c0c174
mireyasantiago <mireyasantiago105@gmail.com>
2017-06-15 11:59:18 -0500

    probando modal

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var contenedorTemas= $("#mostarTema");
 
 var cargarPagina = function () {
   cargarTemas(); // llamamos a la funcion
+  $("formulario").submit(agregarTema);
 }
 
 var cargarTemas = function (){
@@ -16,32 +17,46 @@ var cargarTemas = function (){
 
 var crearTemas = function (tema){
   var plantillaFinal= " ";
-  plantillaFinal += plantillaTema.replace('__temaF__', tema.content)
+  plantillaFinal += plantillaTema.replace('__tema__', tema.content)
   .replace('__autor__', tema.author_name)
   .replace('__respuesta__', tema.responses_count);
   contenedorTemas.append(plantillaFinal); // para mostrar
-console.log(tema);
+  //console.log(tema);
 
 };
-/*hacer plantilla para temas*/
 
+/*hacer plantilla para temas*/
 var plantillaTema = '<tr>' +
-    '<td>__temaF__</td>' +
+    '<td>__tema__</td>' +
     '<td>__autor__</td>' +
     '<td>__respuesta__</td>' +
   '</tr>' ;
 
-/*var creandoTema = function (event){
-  event.preventDefault();
-  var nombre = $("#tema").val();
-  $post(api.url, {
-    content: tema
-  }, funtion(tema){
-    crearTemas(tema)
-  }
-  })
+// se mostrara atravez  de un modal para poder agregar
+var agregarTema = function (e) {
+  e.preventDefault();
+  var temaN = $("#temaN").val();
+  var autorN = $("#autorN").val();
+  var respuestaN = $("#respuestaN").val();
+  $.post(api.url, {
+    /*content: temaN,
+    author_name : autorN,
+    responses_count : respuestaN*/
+
+    "author_name": "temaN",
+        "content": "autorN",
+//        "responses_count": "0"
+
+  }, function (tema) {
+      //$("#myModal").modal("hide");
+    modal();
+    crearTemas(tema);
+  });
+};
+
+var modal= function(){
+  $("#myModal").modal("hide");
 }
-*/
 
 
 $(document).ready(cargarPagina);
