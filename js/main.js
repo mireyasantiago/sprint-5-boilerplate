33a7f0b304f6ea9c7e4d9f3408c9b5930273e226
mireyasantiago <mireyasantiago105@gmail.com>
2017-06-15 10:57:34 -0500

    cambios en lo que muestra los temas

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ var cargarTemas = function (){
 var crearTemas = function (tema){
   var plantillaFinal= " ";
   plantillaFinal += plantillaTema.replace('__temaF__', tema.content)
-  .replace('__autor__', tema.name)
+  .replace('__autor__', tema.author_name)
   .replace('__respuesta__', tema.responses_count);
   contenedorTemas.append(plantillaFinal); // para mostrar
 console.log(tema);
@@ -27,16 +27,21 @@ console.log(tema);
 
 var plantillaTema = '<tr>' +
     '<td>__temaF__</td>' +
-    '<td>__autor__</td>'
+    '<td>__autor__</td>' +
     '<td>__respuesta__</td>' +
   '</tr>' ;
 
-var creandoTema = function (event){
+/*var creandoTema = function (event){
   event.preventDefault();
-  var
+  var nombre = $("#tema").val();
+  $post(api.url, {
+    content: tema
+  }, funtion(tema){
+    crearTemas(tema)
+  }
+  })
 }
-
-
+*/
 
 
 $(document).ready(cargarPagina);
