b42c75066a7b700a0033571fe96ffce2b9eb8d35
mireyasantiago <mireyasantiago105@gmail.com>
2017-06-15 10:34:43 -0500

    mostrando temas con el api

---
+++ js/main.js
@@ -0,0 +1,42 @@
+var api ={
+  url: 'http://examen-laboratoria-sprint-5.herokuapp.com/topics/'
+};
+
+var contenedorTemas= $("#mostarTema");
+
+var cargarPagina = function () {
+  cargarTemas(); // llamamos a la funcion
+}
+
+var cargarTemas = function (){
+  $.getJSON(api.url, function(temas){
+    temas.forEach(crearTemas);
+  });
+};
+
+var crearTemas = function (tema){
+  var plantillaFinal= " ";
+  plantillaFinal += plantillaTema.replace('__temaF__', tema.content)
+  .replace('__autor__', tema.name)
+  .replace('__respuesta__', tema.responses_count);
+  contenedorTemas.append(plantillaFinal); // para mostrar
+console.log(tema);
+
+};
+/*hacer plantilla para temas*/
+
+var plantillaTema = '<tr>' +
+    '<td>__temaF__</td>' +
+    '<td>__autor__</td>'
+    '<td>__respuesta__</td>' +
+  '</tr>' ;
+
+var creandoTema = function (event){
+  event.preventDefault();
+  var
+}
+
+
+
+
+$(document).ready(cargarPagina);
